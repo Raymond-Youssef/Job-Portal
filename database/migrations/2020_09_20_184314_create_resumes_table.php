35b360e4a2a9a1b870d6df4ca783f52f926bdf2d
Raymond Youssef <rimonomega@gmail.com>
2020-09-20 23:53:45 +0200

    More API fixes

--- database/migrations/2020_09_20_184314_create_resumes_table.php
+++ database/migrations/2020_09_20_184314_create_resumes_table.php
@@ -15,7 +15,7 @@ class CreateResumesTable extends Migration
     {
         Schema::create('resumes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->binary('file');
             $table->boolean('default')->default(false);
         });
