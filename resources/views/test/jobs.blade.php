55faae5713ef081be64eb7d84d2a0424745559a1
NewUser <newuser@example.com>
2020-09-25 02:03:19 +0200

    job update

--- resources/views/test/jobs.blade.php
+++ resources/views/test/jobs.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="form">
-                    <h1 class="display-4">{{ __('Job Announcement') }}</h1>
+                    <h2 class="display-4">{{ __('Job Announcement') }}</h2>
                     <form method="POST" action="{{ route('') }}">
                         @csrf
 
@@ -22,7 +22,7 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="job_description" class="col-md-4 col-form-label text-md-right">{{ __('Job job_description') }}</label>
+                            <label for="job_description" class="col-md-4 col-form-label text-md-right">{{ __('Job description') }}</label>
 
                             <div class="col-md-6">
                                 <textarea id="job-description" class="form-control @error('job-description') is-invalid @enderror" name="job-description" value="{{ old('job-decription') }}" required autocomplete="job-description" autofocus>
@@ -48,8 +48,7 @@
                                 @enderror
                             </div>
                         </div>
-                        </div>
-                        </div>
+                        
                         <div class="form-group row">
                             <label for="country" class="col-md-4 col-form-label text-md-right">{{ __('Country') }}</label>
 
@@ -64,8 +63,7 @@
                             </div>
                         </div>
 
-                        </div>
-                        </div>
+                        
                         <div class="form-group row">
                             <label for="city" class="col-md-4 col-form-label text-md-right">{{ __('City') }}</label>
 
@@ -79,15 +77,7 @@
                                 @enderror
                             </div>
                         </div>
-                    
-
-                        
-
-
-                      
-
-
-                        
+                
                    
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
