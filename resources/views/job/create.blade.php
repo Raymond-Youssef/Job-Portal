e2e4715f73d9e419d8c17df934489e2d60601806
Raymond Youssef <rimonomega@gmail.com>
2020-09-25 15:38:33 +0200

    Fixed create job form

--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -22,7 +22,7 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="job_description" class="col-md-4 col-form-label text-md-right">{{ __('Job Description') }}</label>
+                    <label for="job-description" class="col-md-4 col-form-label text-md-right">{{ __('Job Description') }}</label>
 
                     <div class="col-md-6">
                                 <textarea id="job-description" class="form-control @error('description') is-invalid @enderror" name="description" required autocomplete="job-description" autofocus>
