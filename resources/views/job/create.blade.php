6e484b5045e5894ab87aedebaf45a20ce52f6e0c
Raymond Youssef <rimonomega@gmail.com>
2020-09-25 18:11:58 +0200

    Changes in Add job form

--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -43,7 +43,7 @@
 
                     <div class="col-md-6">
                         <input id="skills" type="text" class="form-control @error('skills') is-invalid @enderror" name="skills" value="{{ old('skills') }}" autocomplete="skills" autofocus>
-
+                        <small class="form-text text-muted">Enter skills separated by commas ","</small>
                         @error('skills')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -56,32 +56,24 @@
                 <div class="form-group row">
                     <label for="city" class="col-md-4 col-form-label  text-md-right">{{ __('City') }}</label>
 
-                    <div class="col-md-3">
+                    <div class="col-md-2">
                         <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
-
                         @error('city')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
-
                     </div>
-               
 
-                <!-- <div class="form-group row"> -->
-                    <label for="country" class=" col-form-label text-md-right">{{ __('Country') }}</label>
-
-                    <div class="col-md-3">
+                    <label for="country" class="row-cols-sm-5  col-form-label text-md-right">{{ __('Country') }}</label>
+                    <div class="col-md-2">
                         <input id="country" type="text" class="form-control @error('country') is-invalid @enderror" name="country" value="{{ old('country') }}" required autocomplete="country" autofocus>
-
                         @error('country')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
-
                     </div>
-                </div>
                 </div>
 
                 <div class="form-group row mb-0">
