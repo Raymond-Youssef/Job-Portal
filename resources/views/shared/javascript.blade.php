f35adacef4629d6c000aa5f6fce8a2ba66e40974
Raymond Youssef <rimonomega@gmail.com>
2020-12-11 04:07:22 +0200

    DOne

--- resources/views/shared/javascript.blade.php
+++ resources/views/shared/javascript.blade.php
@@ -11,6 +11,15 @@
 <script src="{{asset('assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
 <script src="{{asset('assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
 <script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.1.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.1.2/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 
 <!-- Template Main JS File -->
 <script src="{{asset('assets/js/main.js')}}"></script>
